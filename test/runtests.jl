fd435baaf39fff1835cf5ec047e7018deae1cfb6
Davide Ferracin <davide.ferracin@protonmail.com>
2025-11-27 17:53:20 +0100

    feat: right-multiplication of LPDO by a tensor
    Implement the `product` method for a tensor and an LDPO in the case
    where the tensor multiplies the LPDO on the right as well.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using Test
 using ITensors, ITensorMPS, LocallyPurifiedDensityOperators, LinearAlgebra
 
+adj(t::ITensor) = swapprime(dag(t), 0 => 1)
+
 @testset "Basic linear algebra" begin
     s = siteinds("Boson", 5; dim=4)
     x = random_mps(s; linkdims=2)
@@ -9,8 +11,13 @@ using ITensors, ITensorMPS, LocallyPurifiedDensityOperators, LinearAlgebra
     u = random_itensor(ComplexF64, s[4]', dag(s[4]))
     t = random_itensor(ComplexF64, s[2]', s[3]', dag(s[2]), dag(s[3]))
     @test tr(ρ) ≈ 1
-    @test inner(x, apply(u, x)) ≈ tr(apply(u, ρ))
-    @test inner(x, apply(t, x)) ≈ tr(apply(t, ρ))
+
+    @test inner(x, apply(u, x)) ≈ tr(apply(u, ρ)) ≈ tr(apply(ρ, u))
+    @test inner(x, apply(t, x)) ≈ tr(apply(t, ρ)) ≈ tr(apply(ρ, t))
+
     @test norm(apply(u, x))^2 ≈ tr(apply(u, ρ; apply_dag=true))
     @test norm(apply(t, x))^2 ≈ tr(apply(t, ρ; apply_dag=true))
+
+    @test norm(apply(adj(u), x))^2 ≈ tr(apply(ρ, u; apply_dag=true))
+    @test norm(apply(adj(t), x))^2 ≈ tr(apply(ρ, t; apply_dag=true))
 end
